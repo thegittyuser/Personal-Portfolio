64fb5dd56976fd5a2638b8f07e193881d4674c6c
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-08-29 20:00:45 +0500

    Personal Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header id="header"></header>
+      <section id="hero"></section>
+      <section id="about"></section>
+      <section id="services"></section>
+      <section id="portfolio"></section>
+      <section id="testimonials"></section>
+      <section id="newsletter"></section>
+      <section id="brands"></section>
+      <footer id="footer"></footer>
+    </>
+  );
+}
+
+export default App;
