fb7eb9dcc7ae285715dfb010491e2bc0857d34ac
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-09-01 19:52:43 +0500

    Header Done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,13 +1,38 @@
 import "./css/header.css";
 
 function Header() {
-    return(
-        <>
-            <section className="header">
-                
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="header">
+        <div className="logo-text">
+          <h2>Talha Mughal</h2>
+        </div>
+        <nav className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Projects</li>
+            <li>Testimonials</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+        <div className="social-icons">
+          <ul>
+            <li>
+              <i class="fab fa-github"></i>
+            </li>
+            <li>
+              <i class="fab fa-linkedin"></i>
+            </li>
+            <li>
+              <i class="fab fa-facebook"></i>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Header;
