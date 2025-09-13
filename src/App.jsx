663fa55c48db7a7bc3b93fc316600bb4edf71f8f
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-09-13 20:05:36 +0500

    ReactBits

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,37 @@
-import Header from "./Components/Header.jsx";
+// import Header from "./Components/Header.jsx";
 import Hero from "./Components/Hero.jsx";
-import Skills from "./Components/Skills.jsx";
-import Services from "./Components/Services.jsx";
-import Contact from "./Components/Contact.jsx";
-import Footer from "./Components/Footer.jsx";
+// import Skills from "./Components/Skills.jsx";
+// import Services from "./Components/Services.jsx";
+// import Contact from "./Components/Contact.jsx";
+// import Footer from "./Components/Footer.jsx";
+import Threads from "./bits/Threads.jsx";
+import Prism from "./bits/Prism.jsx";
 
 function App() {
   return (
     <>
-      <Header />
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <Prism
+          animationType="rotate"
+          timeScale={0.5}
+          height={3.5}
+          baseWidth={5.5}
+          scale={3.6}
+          hueShift={0}
+          colorFrequency={1}
+          noise={0.5}
+          glow={1}
+        />
+      </div>
+      {/* {/* <Header /> */}
       <Hero />
-      <Skills />
-      <Services />
-      <Contact />
-      <Footer />
+      {/* <Skills /> */}
+      {/* <Services /> */}
+      {/* <Contact /> */}
+      {/* <Footer /> */}
+      <div style={{ width: "100%", height: "600px", position: "relative" }}>
+        <Threads amplitude={1} distance={0} enableMouseInteraction={true} />
+      </div>
     </>
   );
 }
